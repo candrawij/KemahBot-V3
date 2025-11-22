63111cfc77326d97deaee88d5dc833654ce2cb0c
candrawij <candrawijayanto340@gmail.com>
2025-11-22 13:30:30 +0700

    3.0.3

--- api/app.py
+++ api/app.py
@@ -150,4 +150,4 @@ if __name__ == "__main__":
     dengan 'python main.py' (berguna untuk debugging).
     """
     print("--- Menjalankan server Uvicorn (mode debug) ---")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
--- chatbot_ui/orchestrator.py
+++ chatbot_ui/orchestrator.py
@@ -36,7 +36,7 @@ except Exception as e:
     model = None
 
 # URL tempat API FastAPI (Fase 1) Anda berjalan
-RETRIEVAL_API_URL = "https://kaira21-campground-api.hf.space/docs#/default/search_kemah_search_post"
+RETRIEVAL_API_URL = "https://Kaira21-campground-api.hf.space/search"
 
 # =====================================================================
 # FUNGSI 1: PANGGIL LLM (Ekstraksi Keyword)
